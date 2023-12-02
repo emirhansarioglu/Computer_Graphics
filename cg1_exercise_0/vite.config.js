fed7302ee471fba31005a9d1d1db244d4778552d
Emirhan Sarıoğlu <emirhansarioglu6153@gmail.com>
2023-12-02 16:15:40 +0100

    Changed the directory

--- cg1_exercise_0/vite.config.js
+++
@@ -1,9 +0,0 @@
-import checker from 'vite-plugin-checker'
-export default {
-  plugins: [checker({ typescript: true })], // e.g. use TypeScript check
-  resolve: { preserveSymlinks: true },
-  assetsInclude: ['**/*.jpg', '**/*.glsl','**/*.obj','**/*.ply','**/*.off'],
-  build: {
-    sourcemap: true
-  }
-}
